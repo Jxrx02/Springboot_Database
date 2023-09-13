d7d0dd698615aa31cc83c00e7bfb0850687fc90b
JeremiasEssert <j.essert@sap.com>
2023-09-13 10:21:04 +0200

    setting up

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,23 @@
+
+h
+8demo/src/main/java/com/example/demo/DemoApplication.java,b\b\bbb92027e3abb2f6f7442486303df9ca1e7dd41f
+‚
+Rdemo/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java,7\a\7a6dbafeaab56bca78b83372266ed45b9a3dff56
+x
+Hdemo/src/main/java/com/example/servingwebcontent/GreetingController.java,a\5\a5b4e20fcc6265f4eac2141a96cf03a43ebe735b
+w
+Gdemo/src/main/java/com/example/servingwebcontent/ElementController.java,7\6\769622b9c1fbde2b4b1f61e48ec54c64ff65c668
+;
+demo/create,3\f\3f4266858bdea4bb78a683232e8d670a94bc03ff
+D
+demo/insert_data.sql,c\0\c01924d469bb095f24bbb6264964beeefa6a01bc
+9
+	demo/mvnw,7\6\76db9d6e678bc8b7f3bead58da97f140068b0806
+=
+demo/mvnw.cmd,4\3\437e70af9793a466618b92893f8c7d9117237271
+h
+8.idea/libraries/Maven__org_xerial_sqlite_jdbc_3_43_1.xml,1\3\13f15995b9cfd493dcde853e674a389d4670cd82
+<
+demo/pom.xml,f\1\f1bf400bc0784549511ea07030d3a798eb8c0511
+F
+demo/create_tables.sql,d\b\dbbc9fcac8fd69a631afde716551d04a715c2941
---
+++ demo/src/main/java/com/example/servingwebcontent/ElementController.java
@@ -0,0 +1,146 @@
+package com.example.servingwebcontent;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class ElementController  {
+
+    private List<String> elements = new ArrayList<>();
+    private List<String> id = new ArrayList<>();
+    private List<String> vorname = new ArrayList<>();
+    private List<String> nachname = new ArrayList<>();
+    String url = "jdbc:sqlite:C:\\Users\\I569045\\OneDrive - SAP SE\\Praxisphase 2\\springboot with html\\demo\\src\\main\\resources\\database\\benutzer";
+
+    @GetMapping("/t")
+    public String showList(Model model) throws SQLException {
+        model.addAttribute("elements", elements);
+        loadDatabaseToList("SELECT * FROM nutzer");
+
+        model.addAttribute("ids", id);
+        model.addAttribute("vornames", vorname);
+        model.addAttribute("nachnames", nachname);
+
+        return "list_page";
+    }
+
+    private void loadDatabaseToList(String sqlStatement) {
+        id.clear();
+        vorname.clear();
+        nachname.clear();
+        // JDBC-Verbindungsdaten
+
+        Connection conn = null;
+
+        try {
+            // JDBC-Treiber explizit laden
+            Class.forName("org.sqlite.JDBC");
+
+            // Verbindung zur Datenbank herstellen
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Daten geladen.");
+
+            // SQL-Abfrage ausführen
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sqlStatement);
+
+            // Daten in Liste
+            while (rs.next()) {
+                id.add(String.valueOf(rs.getInt("id")));
+                vorname.add(rs.getString("vorname"));
+                nachname.add(rs.getString("nachname"));
+            }
+
+            // Verbindung schließen
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @PostMapping("/addElement")
+    public String processFormDataAddElement(
+            @RequestParam("idInput") String id,
+            @RequestParam("vornameInput") String vorname,
+            @RequestParam("nachnameInput") String nachname,
+            Model model) {
+
+        this.id.add(id);
+        this.vorname.add(vorname);
+        this.nachname.add(nachname);
+
+        updateDatabase();
+        return "redirect:/t";
+    }
+    private void updateDatabase () {
+        // JDBC-Verbindungsdaten
+        Connection conn = null;
+        try {
+            // JDBC-Treiber explizit laden
+            Class.forName("org.sqlite.JDBC");
+
+            // Verbindung zur Datenbank herstellen
+            conn = DriverManager.getConnection(url);
+
+            // SQL-Abfrage ausführen
+            Statement stmt = conn.createStatement();
+            //ResultSet rs = stmt.executeQuery(sqlStatement);
+
+            //tabelle leeren
+            stmt.execute("DELETE FROM nutzer;");
+
+            String sqlStatement = "INSERT INTO nutzer (id, vorname, nachname) VALUES ";
+
+            for (int i = 0; i < id.size(); i++) {
+                sqlStatement += "(" + id.get(i) + ", '" + vorname.get(i) + "', '" + nachname.get(i) + "')";
+
+                // Füge ein Komma und Leerzeichen hinzu, wenn es nicht das letzte Element ist
+                if (i < id.size() - 1) {
+                    sqlStatement += ", ";
+                }
+            }
+            stmt.executeUpdate(sqlStatement);
+
+            System.out.println("Daten überschrieben.");
+            // Verbindung schließen
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @GetMapping("/deleteAll")
+    public String deleteAllElements() {
+        this.id.clear();
+        this.vorname.clear();
+        this.nachname.clear();
+        Connection conn = null;
+        try {
+            // JDBC-Treiber explizit laden
+            Class.forName("org.sqlite.JDBC");
+
+            // Verbindung zur Datenbank herstellen
+            conn = DriverManager.getConnection(url);
+
+            // SQL-Abfrage ausführen
+            Statement stmt = conn.createStatement();
+            //ResultSet rs = stmt.executeQuery(sqlStatement);
+
+            //tabelle leeren
+            stmt.execute("DELETE FROM nutzer;");
+            System.out.println("Daten gelöscht.");
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "redirect:/t";
+    }
+}
